dd64e1da786e04106242216279750fcb752069e0
suryathejareddy <surya.seggari@gmail.com>
2019-01-06 18:10:03 +0530

    firstapp
    1/6/19

---
+++ SampleApp/app/src/main/java/com/android/stepinleads/sampleapp/Firstclass.java
@@ -0,0 +1,13 @@
+package com.android.stepinleads.sampleapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Firstclass extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_firstclass);
+    }
+}
