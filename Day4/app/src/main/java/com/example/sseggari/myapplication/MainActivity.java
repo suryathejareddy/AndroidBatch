f3ec5090305eb1ee2eeb956746af1f9ee5cda635
Surya Theja Reddy Seggari <sseggari@hostanalytics.com>
2019-01-27 11:23:27 +0530

    changes1

--- Day4/app/src/main/java/com/example/sseggari/myapplication/MainActivity.java
+++ Day4/app/src/main/java/com/example/sseggari/myapplication/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.sseggari.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.CompoundButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        t=findViewById(R.id.textview);
+        c=findViewById(R.id.checkBox);
+        c.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                t.setTextColor(getColor(R.color.colorPrimaryDark));
+                t.setTextSize(20);
+                t.setBackgroundColor(getColor(R.color.colorPrimary));
+                t.setText( c.getText().toString());
+            }
+        });
+
     }
 }
