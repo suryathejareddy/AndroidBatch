f417035b39ce00324488670f44f8ccf6a84a2227
suryathejareddy <surya.seggari@gmail.com>
2019-03-10 11:01:41 +0530

    otpsampleapp

---
+++ OtpCheckApp/app/src/main/java/cloneapps/androidcourse/stepinleads/otpcheckapp/MainActivity.java
@@ -0,0 +1,14 @@
+package cloneapps.androidcourse.stepinleads.otpcheckapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
